1e68266392406229725c66e75202ceb2edaae0e9
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2023-12-05 01:13:14 +0000

    added basic random level generation

--- src/level/mod.rs
+++ src/level/mod.rs
@@ -1,14 +1,22 @@
 use bevy::prelude::*;
 use bevy_rapier3d::{prelude::*, rapier::geometry::CollisionEventFlags};
+use rand::Rng;
 
 use crate::{COLLISION_GROUP_ENEMY, COLLISION_GROUP_LEVEL, COLLISION_GROUP_PROJECTILES};
+
+const LEVEL_SIZE: f32 = 200.0;
+const COLUMN_SIZE: f32 = 5.0;
+const COLUMN_HIGHT: f32 = 10.0;
+const GRID_SIZE: usize = (LEVEL_SIZE / COLUMN_SIZE) as usize;
+const FILL_AMOUNT: f32 = 0.02;
+const STRIP_LENGTH: u32 = 3;
 
 pub struct LevelPlugin;
 
 impl Plugin for LevelPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, init);
-        app.add_systems(PostStartup, generate_level);
+        app.add_systems(PostStartup, spawn_level);
         app.add_systems(Update, collision_level_object_projectiles);
     }
 }
@@ -18,18 +26,22 @@ pub struct LevelObject;
 
 #[derive(Resource)]
 struct LevelResources {
+    floor_mesh: Handle<Mesh>,
     floor_material: Handle<StandardMaterial>,
-    wall_material: Handle<StandardMaterial>,
+    column_mesh: Handle<Mesh>,
+    column_material: Handle<StandardMaterial>,
+    portal_mesh: Handle<Mesh>,
+    portal_material: Handle<StandardMaterial>,
 }
 
 #[derive(Bundle)]
-struct LevelObjectBundle {
-    pbr_bundle: PbrBundle,
-    collider: Collider,
-    collision_groups: CollisionGroups,
-    active_collision_types: ActiveCollisionTypes,
-    rigid_body: RigidBody,
-    level_object: LevelObject,
+pub struct LevelObjectBundle {
+    pub pbr_bundle: PbrBundle,
+    pub collider: Collider,
+    pub collision_groups: CollisionGroups,
+    pub active_collision_types: ActiveCollisionTypes,
+    pub rigid_body: RigidBody,
+    pub level_object: LevelObject,
 }
 
 impl Default for LevelObjectBundle {
@@ -69,80 +81,159 @@ impl LevelObjectBundle {
     }
 }
 
-fn init(mut commands: Commands, mut materials: ResMut<Assets<StandardMaterial>>) {
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum CellType {
+    Empty,
+    Portal,
+    Column,
+}
+
+fn init(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let floor_mesh = meshes.add(shape::Box::new(LEVEL_SIZE, LEVEL_SIZE, 1.0).into());
     let floor_material = materials.add(Color::GRAY.into());
-    let wall_material = materials.add(Color::DARK_GRAY.into());
+
+    let column_mesh = meshes.add(shape::Box::new(COLUMN_SIZE, COLUMN_SIZE, COLUMN_HIGHT).into());
+    let column_material = materials.add(Color::DARK_GRAY.into());
+
+    let portal_mesh = meshes.add(shape::Box::new(COLUMN_SIZE, COLUMN_SIZE, COLUMN_HIGHT).into());
+    let portal_material = materials.add(Color::BLUE.into());
 
     commands.insert_resource(LevelResources {
+        floor_mesh,
         floor_material,
-        wall_material,
+        column_mesh,
+        column_material,
+        portal_mesh,
+        portal_material,
     });
 }
 
-fn generate_level(
-    level_resources: Res<LevelResources>,
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-) {
+fn generate_level() -> [[CellType; GRID_SIZE]; GRID_SIZE] {
+    let mut rng = rand::thread_rng();
+
+    let mut grid = [[CellType::Empty; GRID_SIZE]; GRID_SIZE];
+
+    let random_exit_top = rng.gen_range(1..GRID_SIZE - 1);
+    for x in 0..GRID_SIZE {
+        grid[0][x] = CellType::Column;
+    }
+    grid[0][random_exit_top] = CellType::Portal;
+
+    let random_exit_bot = rng.gen_range(1..GRID_SIZE - 1);
+    for x in 0..GRID_SIZE {
+        grid[GRID_SIZE - 1][x] = CellType::Column;
+    }
+    grid[GRID_SIZE - 1][random_exit_bot] = CellType::Portal;
+
+    let random_exit_left = rng.gen_range(1..GRID_SIZE - 1);
+    (0..GRID_SIZE).for_each(|y| {
+        grid[y][0] = CellType::Column;
+    });
+    grid[random_exit_left][0] = CellType::Portal;
+
+    let random_exit_right = rng.gen_range(1..GRID_SIZE - 1);
+    (0..GRID_SIZE).for_each(|y| {
+        grid[y][GRID_SIZE - 1] = CellType::Column;
+    });
+    grid[random_exit_right][GRID_SIZE - 1] = CellType::Portal;
+
+    let fill_cells = (GRID_SIZE as f32 * GRID_SIZE as f32 * FILL_AMOUNT) as u32;
+    let num_strips = fill_cells / STRIP_LENGTH;
+    for _ in 0..num_strips {
+        let random_cell_x = rng.gen_range(2..GRID_SIZE - 2);
+        let random_cell_y = rng.gen_range(2..GRID_SIZE - 2);
+        grid[random_cell_y][random_cell_x] = CellType::Column;
+
+        let mut current_x: i32 = random_cell_x as i32;
+        let mut current_y: i32 = random_cell_y as i32;
+
+        for _ in 0..STRIP_LENGTH {
+            let mods = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+            let valid_pos = mods
+                .iter()
+                .filter_map(|(x_mod, y_mod)| {
+                    let (next_x, next_y) = (current_x + x_mod, current_y + y_mod);
+                    if next_x < 2
+                        || GRID_SIZE as i32 - 2 <= next_x
+                        || next_y < 2
+                        || GRID_SIZE as i32 - 2 <= next_y
+                    {
+                        None
+                    } else {
+                        Some((next_x, next_y))
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            if valid_pos.is_empty() {
+                break;
+            }
+
+            let random_cell = rng.gen_range(0..valid_pos.len());
+            (current_x, current_y) = valid_pos[random_cell];
+            grid[current_y as usize][current_x as usize] = CellType::Column;
+        }
+    }
+
+    // check if there are some "trapped" places and remove them
+    for y in 2..GRID_SIZE - 2 {
+        for x in 2..GRID_SIZE - 2 {
+            if grid[y][x] == CellType::Empty
+                && grid[y - 1][x] == CellType::Column
+                && grid[y + 1][x] == CellType::Column
+                && grid[y][x + 1] == CellType::Column
+                && grid[y][x - 1] == CellType::Column
+            {
+                grid[y][x] = CellType::Column;
+            }
+        }
+    }
+
+    grid
+}
+
+fn spawn_level(level_resources: Res<LevelResources>, mut commands: Commands) {
+    let grid = generate_level();
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, cell) in row.iter().enumerate() {
+            let x_pos = (-LEVEL_SIZE / 2.0) + COLUMN_SIZE * x as f32 + COLUMN_SIZE / 2.0;
+            let y_pos = (-LEVEL_SIZE / 2.0) + COLUMN_SIZE * y as f32 + COLUMN_SIZE / 2.0;
+            let z_pos = COLUMN_HIGHT / 2.0;
+            let translation = Vec3::new(x_pos, y_pos, z_pos);
+
+            match cell {
+                CellType::Column => {
+                    commands.spawn(LevelObjectBundle::new(
+                        level_resources.column_mesh.clone(),
+                        level_resources.column_material.clone(),
+                        Transform::from_translation(translation),
+                        Collider::cuboid(COLUMN_SIZE / 2.0, COLUMN_SIZE / 2.0, COLUMN_HIGHT / 2.0),
+                    ));
+                }
+                CellType::Portal => {
+                    commands.spawn(LevelObjectBundle::new(
+                        level_resources.portal_mesh.clone(),
+                        level_resources.portal_material.clone(),
+                        Transform::from_translation(translation),
+                        Collider::cuboid(COLUMN_SIZE / 2.0, COLUMN_SIZE / 2.0, COLUMN_HIGHT / 2.0),
+                    ));
+                }
+                CellType::Empty => {}
+            }
+        }
+    }
+
     // floor
-    let dimentions = Vec3::new(500.0, 500.0, 1.0);
-    let mesh = meshes.add(shape::Box::new(dimentions.x, dimentions.y, dimentions.z).into());
     commands.spawn(LevelObjectBundle::new(
-        mesh,
+        level_resources.floor_mesh.clone(),
         level_resources.floor_material.clone(),
         Transform::default(),
-        Collider::cuboid(dimentions.x / 2.0, dimentions.y / 2.0, dimentions.z / 2.0),
-    ));
-
-    // +X test wall
-    let dimentions = Vec3::new(1.0, 100.0, 10.0);
-    let mesh = meshes.add(shape::Box::new(dimentions.x, dimentions.y, dimentions.z).into());
-    let transform = Transform::from_translation(Vec3::new(20.0, 0.0, 5.0));
-    commands.spawn(LevelObjectBundle::new(
-        mesh.clone(),
-        level_resources.wall_material.clone(),
-        transform,
-        Collider::cuboid(dimentions.x / 2.0, dimentions.y / 2.0, dimentions.z / 2.0),
-    ));
-
-    // +X wall
-    let dimentions = Vec3::new(1.0, 500.0, 10.0);
-    let mesh = meshes.add(shape::Box::new(dimentions.x, dimentions.y, dimentions.z).into());
-    let transform = Transform::from_translation(Vec3::new(250.0, 0.0, 5.0));
-    commands.spawn(LevelObjectBundle::new(
-        mesh.clone(),
-        level_resources.wall_material.clone(),
-        transform,
-        Collider::cuboid(dimentions.x / 2.0, dimentions.y / 2.0, dimentions.z / 2.0),
-    ));
-
-    // -X wall
-    let transform = Transform::from_translation(Vec3::new(-250.0, 0.0, 5.0));
-    commands.spawn(LevelObjectBundle::new(
-        mesh,
-        level_resources.wall_material.clone(),
-        transform,
-        Collider::cuboid(dimentions.x / 2.0, dimentions.y / 2.0, dimentions.z / 2.0),
-    ));
-
-    // +Y wall
-    let dimentions = Vec3::new(500.0, 1.0, 10.0);
-    let mesh = meshes.add(shape::Box::new(dimentions.x, dimentions.y, dimentions.z).into());
-    let transform = Transform::from_translation(Vec3::new(0.0, 250.0, 5.0));
-    commands.spawn(LevelObjectBundle::new(
-        mesh.clone(),
-        level_resources.wall_material.clone(),
-        transform,
-        Collider::cuboid(dimentions.x / 2.0, dimentions.y / 2.0, dimentions.z / 2.0),
-    ));
-
-    // -Y wall
-    let transform = Transform::from_translation(Vec3::new(0.0, -250.0, 5.0));
-    commands.spawn(LevelObjectBundle::new(
-        mesh,
-        level_resources.wall_material.clone(),
-        transform,
-        Collider::cuboid(dimentions.x / 2.0, dimentions.y / 2.0, dimentions.z / 2.0),
+        Collider::cuboid(LEVEL_SIZE / 2.0, LEVEL_SIZE / 2.0, 0.5),
     ));
 }
 
