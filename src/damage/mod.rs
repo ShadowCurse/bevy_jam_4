fb619d8119589dc5a446f8ba48ce5f1e3848a86d
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2023-12-09 20:46:46 +0000

    added damage events

--- src/damage/mod.rs
+++ src/damage/mod.rs
@@ -7,10 +7,17 @@ pub struct DamagePlugin;
 
 impl Plugin for DamagePlugin {
     fn build(&self, app: &mut App) {
+        app.add_event::<DamageEvent>();
         app.add_event::<KillEvent>();
 
         app.add_systems(Update, apply_damage.run_if(in_state(GlobalState::InGame)));
     }
+}
+
+#[derive(Event)]
+pub struct DamageEvent {
+    pub entity: Entity,
+    pub direction: Vec3,
 }
 
 #[derive(Event)]
@@ -29,11 +36,12 @@ pub struct Health {
 }
 
 fn apply_damage(
-    projectiles: Query<&Damage, With<Projectile>>,
+    projectiles: Query<(&Damage, &Transform), With<Projectile>>,
     mut commands: Commands,
     mut kill_events: EventWriter<KillEvent>,
+    mut damage_events: EventWriter<DamageEvent>,
     mut collision_events: EventReader<CollisionEvent>,
-    mut entities: Query<(Entity, &mut Health)>,
+    mut entities: Query<(Entity, &Transform, &mut Health)>,
 ) {
     for collision_event in collision_events.read() {
         let (collider_1, collider_2, flags) = match collision_event {
@@ -44,29 +52,34 @@ fn apply_damage(
             return;
         }
 
-        let (projectile_damage, (entity, mut entity_health)) =
-            if let Ok(p) = projectiles.get(*collider_1) {
-                let e = if let Ok(e) = entities.get_mut(*collider_2) {
-                    e
-                } else {
-                    continue;
-                };
-                (p, e)
-            } else if let Ok(p) = projectiles.get(*collider_2) {
-                let e = if let Ok(e) = entities.get_mut(*collider_1) {
-                    e
-                } else {
-                    continue;
-                };
-                (p, e)
+        let (
+            (projectile_damage, projectile_transform),
+            (entity, entity_transform, mut entity_health),
+        ) = if let Ok(p) = projectiles.get(*collider_1) {
+            let e = if let Ok(e) = entities.get_mut(*collider_2) {
+                e
             } else {
                 continue;
             };
+            (p, e)
+        } else if let Ok(p) = projectiles.get(*collider_2) {
+            let e = if let Ok(e) = entities.get_mut(*collider_1) {
+                e
+            } else {
+                continue;
+            };
+            (p, e)
+        } else {
+            continue;
+        };
 
         entity_health.health -= projectile_damage.damage;
         if entity_health.health <= 0 {
             commands.get_entity(entity).unwrap().remove::<Health>();
             kill_events.send(KillEvent { entity });
+        } else {
+            let direction = projectile_transform.translation - entity_transform.translation;
+            damage_events.send(DamageEvent { entity, direction });
         }
     }
 }
